9870ac506139302cdf126f46f82ec47078b8c0f4
dilshanamarasinghe1 <damarasinghe@mitrai.com>
2017-02-21 01:56:11 +0530

    added nodemon

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-// var http= require('http');
-
-//  http.createServer(function(req, res){ 
-
-//      res.writeHead(200, {'ContentType': 'text/plain'}
-//      );
  
-//      res.end('Hello World\n');
-//  }).listen(3000, '127.0.0.1');
-
-
-
-  
 var express = require('express');
 
+var app= express();
+
+app.get('/',function(req, res){
+
+//res.send('Hello world');
+res.json({hello:'world'});
+});
+
+var server = app.listen(3000, function(){
+
 console.log('Server running at http://127.0.0.1:3000/');
+
+});
+
