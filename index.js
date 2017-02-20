588cdd78408ce0fec044d3e9c373398984419d20
dilshanamarasinghe1 <damarasinghe@mitrai.com>
2017-02-21 01:24:17 +0530

    added new items

---
+++ index.js
@@ -0,0 +1,16 @@
+// var http= require('http');
+
+//  http.createServer(function(req, res){ 
+
+//      res.writeHead(200, {'ContentType': 'text/plain'}
+//      );
+ 
+//      res.end('Hello World\n');
+//  }).listen(3000, '127.0.0.1');
+
+
+
+  
+var express = require('express');
+
+console.log('Server running at http://127.0.0.1:3000/');
