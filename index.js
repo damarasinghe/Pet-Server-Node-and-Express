c75f4f673e4aaf73d65ca790de0c99dc1f57dad6
dilshanamarasinghe1 <damarasinghe@mitrai.com>
2017-02-21 09:35:31 +0530

    aded body parser and lodash

--- index.js
+++ index.js
@@ -2,13 +2,16 @@
 var express = require('express');
 
 var app= express();
+var bodyParser= require('body-parser');
 
-app.get('/',function(req, res){
 
-//res.send('Hello world');
-res.json({hello:'world'});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
 
-});
+extended:true
+}));
+
+var cats=require('./cats.js')(app);
 
 var server = app.listen(3000, function(){
 
