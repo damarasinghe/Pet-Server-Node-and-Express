ffbfcfa9bab07645f89a178bdddfc013e97f2a37
dilshanamarasinghe1 <damarasinghe@mitrai.com>
2017-02-27 09:44:29 +0530

    addeed mongoose and mongo to persist

--- cat_server.js
+++ cat_server.js
@@ -4,7 +4,11 @@ var express = require('express');
 var app= express();
 var bodyParser= require('body-parser');
 
-var port = process.env.PORT || 8080;
+var mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost/cats');
+
+
+//var port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -12,9 +16,9 @@ app.use(bodyParser.urlencoded({
 extended:true
 }));
 
-var cats=require('./cats.js')(app);
+var cats=require('.routes/cat.js')(app);
 
-var server = app.listen(port, function(){
+var server = app.listen(3000, function(){
 
 console.log('Server running at http://127.0.0.1:3000/');
 
---
+++ routes/pet.js
@@ -0,0 +1,23 @@
+var r = require('request').defaults({
+    json: true
+});
+
+module.exports = function(app) {
+
+app.get('/pets',function(req, res) {
+
+r({uri:'http://;ocalhost:3001/dog'}, function(error, response, body){
+    if(!error && response.statusCode==200){
+         res.json(body);
+
+    }else{
+        res.send(response.statusCode);
+
+    }
+
+}); 
+
+
+});
+
+}
